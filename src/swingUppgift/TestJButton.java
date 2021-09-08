83e11eb7f834cda05c13aa594b96e241116430b9
KasjanK <kasol99@gmail.com>
2021-09-08 14:05:11 +0200

    v1

---
+++ src/swingUppgift/TestJButton.java
@@ -0,0 +1,40 @@
+package swingUppgift;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class TestJButton {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		ImageIcon img = new ImageIcon("C:\\Users\\kowal\\Downloads\\icons8-puzzle-48.png");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 400);
+		frame.setTitle("Test Button");
+		frame.setVisible(true);
+		frame.getContentPane().setBackground(new Color(255, 100, 100));
+		frame.setIconImage(img.getImage());
+		
+		JButton btn = new JButton();
+		btn.setBounds(100, 150, 200, 50);
+		btn.setText("Calculate");
+		
+		JLabel label = new JLabel();
+		label.setText("27 + 3");
+		frame.add(btn);
+		frame.add(label);
+		
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("30");
+			}
+		});
+		
+	}
+}
